be1fe7582754b203f9146d2b792132285571cff8
dingliang <master8902@163.com>
2015-08-30 18:52:01 +0800

    dlcommit

--- src/com/niuxin/action/UserAction.java
+++ src/com/niuxin/action/UserAction.java
@@ -3,20 +3,27 @@ package com.niuxin.action;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sf.json.JSONArray;
 import net.sf.json.JSONException;
 import net.sf.json.JSONObject;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
 
+import com.niuxin.bean.Share;
+import com.niuxin.bean.ShareSelect;
 import com.niuxin.bean.User;
+import com.niuxin.bean.UserFriend;
+import com.niuxin.service.IUserFriendService;
 import com.niuxin.service.IUserService;
+import com.niuxin.util.GetJsonString;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class UserAction extends ActionSupport {
@@ -27,6 +34,9 @@ public class UserAction extends ActionSupport {
 
 	@Resource
 	private IUserService userService;
+	@Resource
+	private IUserFriendService userFriendService;
+
 
 	public void countAll() {
 		response.setContentType("text/plain");
@@ -54,6 +64,50 @@ public class UserAction extends ActionSupport {
 		}
 	}
 
+	public void insertFriend() {
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("utf-8");
+		
+		String str = new GetJsonString().getJsonString(request);
+		if(str!=null&&!str.equals("")){
+			// 用json进行解析
+			JSONArray jsar = JSONArray.fromObject(str);
+			List<UserFriend> userfriendlist = null;
+			for (int i = 0; i < jsar.size(); i++) {
+				UserFriend userSelect = new UserFriend();
+				JSONObject json_data = jsar.getJSONObject(i);				
+				if(i==0){
+					userfriendlist = userFriendService.selectByUserid(json_data.getInt("selfid"));
+				}	
+				if(userfriendlist!=null){
+					int mark = 0;
+					for(int j = 0;j<userfriendlist.size();j++){						
+						if(userfriendlist.get(j).getUserFriendId()==json_data.getInt("friendid")){						
+							mark =1;
+							break;
+						}	
+					}
+					if(mark==0){
+						userSelect.setUserSelfId(json_data.getInt("selfid"));
+						userSelect.setUserFriendId(json_data.getInt("friendid"));
+						userFriendService.insert(userSelect);
+					}
+				}
+			
+			}
+		}
+		
+		String json = "";
+		try {
+			response.getWriter().write(json);
+			response.getWriter().flush();
+			response.getWriter().close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
 	public void insert() {
 	//	System.out.println("开始插入记录？？？？？》》》》》》");
 		User user = new User();
